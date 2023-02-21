881370a7de5616ae28b3e66dcd8c09f32f1095d6
abangdsm <id.reversal@gmail.com>
2023-02-21 11:28:09 +0700

    Menginstall template engine

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require("express")
 const mysql = require("mysql")
 
-const app = express();
+const app = express()
 
-app.get("/", (req, res) => {
-    res.send()
+const db = mysql.createConnection({
+    host: "localhost",
+    database: "ucapan",
+    user: "root",
+    password: ""
+})
+
+db.connect((err) => {
+    if(err) throw err
+    console.log('Database Connected!')
+
+    const sql = "SELECT * FROM tbucapan"
+    db.query(sql, (err, result) => {
+        const users = JSON.parse(JSON.stringify(result))
+        console.log('Hasil database -> ', users);
+        app.get("/", (req, res) => {
+            res.send(users)
+        })
+    })
+
 })
 
 app.listen(8000, () => {
-    console.log('Server Ready!');
-});
+    console.log('Server Ready!')
+})
